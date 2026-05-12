167fb50e09574a5aefd8067e9d87622371bc604d
Roni <roni.ribeiro.n01@gmail.com>
2026-05-11 23:47:27 -0300

    chore: initialize Spring Boot project structure and core dependencies

---
+++ src/main/java/com/rbr/tiktask/TiktaskApplication.java
@@ -0,0 +1,13 @@
+package com.rbr.tiktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbr/tiktask/TiktaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbr.tiktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
